c6fb7e234ffbf8a726d100f6f54fa0df8476842a
Jeffrey van Pelt <jeff@vanpelt.one>
2025-01-09 03:40:40 +0100

    Added admin and banlist

--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ mkdir -p /data/{saves,mods}
 # Test if we have settings files, if not, copy them
 if ! test -f /data/map-gen-settings.json
 then
-    echo "map-gen-settings not found, creating new"
+    echo "map-gen-settings not found, copy template"
     cp /game/data/map-gen-settings.example.json /data/map-gen-settings.json
 fi
 
 if ! test -f /data/map-settings.json
 then
-    echo "map-settings not found, creating new"
+    echo "map-settings not found, copy template"
     cp /game/data/map-settings.example.json /data/map-settings.json
 fi
 
@@ -21,6 +21,18 @@ if ! test -f /data/server-settings.json
 then
     echo "server-settings not found, creating new"
     cp /game/data/server-settings.example.json /data/server-settings.json
+fi
+
+if ! test -f /data/server-adminlist.json
+then
+    echo "server-adminlist not found, creating new empty list"
+    echo "[]" > /data/server-adminlist.json
+fi
+
+if ! test -f /data/server-banlist.json
+then
+    echo "server-banlist not found, creating new empty list"
+    echo "[]" > /data/server-banlist.json
 fi
 
 # Test if we have a save, if not, create it
@@ -31,4 +43,4 @@ then
 fi
 
 echo "Starting server"
-/game/bin/x64/factorio --start-server /data/saves/server.zip --server-settings /data/server-settings.json --mod-directory /data/mods
+/game/bin/x64/factorio --start-server /data/saves/server.zip --server-settings /data/server-settings.json --mod-directory /data/mods  --server-adminlist /data/server-adminlist.json --server-banlist /data/server-banlist.json
